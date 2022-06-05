ea941c5b6958e21213297e7a33dc8b3f56df6e6d
feynoobs <p.w.feeniej@gmail.com>
2022-06-06 05:25:02 +0900

    fuga

--- boot/boot.c
+++ boot/boot.c
@@ -4,7 +4,27 @@
 EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *table)
 {
     InitializeLib(image, table);
-    Print(L"Hello, world!\n");
+
+    EFI_BOOT_SERVICES *BS = table->BootServices;
+    EFI_RUNTIME_SERVICES *RS = table->RuntimeServices;
+    EFI_STATUS status;
+    EFI_HANDLE handle;
+    EFI_LOADED_IMAGE *img;
+    EFI_DEVICE_PATH *imgpath;
+    EFI_BLOCK_IO *blkio;
+
+    EFI_GUID LoadedImageGUID = LOADED_IMAGE_PROTOCOL;
+    EFI_GUID DevicePathGUID = DEVICE_PATH_PROTOCOL;
+    EFI_GUID BlockIoProtocolGUID = BLOCK_IO_PROTOCOL;
+
+    Print(L"TinyX Boot Loader\n");
+    status = uefi_call_wrapper(BS->HandleProtocol, 3, image, &LoadedImageGUID, (VOID **)&img);
+    if (status == EFI_SUCCESS) {
+        status = uefi_call_wrapper(img->DeviceHandle, 2, &DevicePathGUID, (VOID **)&imgpath);
+        if (status == EFI_SUCCESS) {
+            status = uefi_call_wrapper(BS->LocateHandle, 3, ByProtocol, &BlockIoProtocolGUID, (VOID **)&blkio);
+        }
+    }
 
     return EFI_SUCCESS;
 }
