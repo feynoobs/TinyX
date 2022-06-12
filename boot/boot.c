3c471dc5b23830fbb13ce5082b5ed864df9cafda
feynoobs <p.w.feeniej@gmail.com>
2022-06-12 22:02:26 +0900

    なんとなく

--- boot/boot.c
+++ boot/boot.c
@@ -21,6 +21,44 @@ Free(EFI_BOOT_SERVICES *BS, VOID *memory)
     uefi_call_wrapper(BS->FreePool, 1, memory);
 }
 
+INTN
+Memcmp(const VOID *s1, const VOID *s2, UINTN length)
+{
+    const UINT8 *p1 = (UINT8 *)s1;
+    const UINT8 *p2 = (UINT8 *)s2;
+    INTN ret;
+
+    while (length > 0) {
+        --length;
+        ret = *p1 - *p2;
+        if (ret != 0) {
+            break;
+        }
+    }
+
+    return ret;
+}
+
+BOOLEAN
+IsNodeMatch(EFI_DEVICE_PATH *imgPath, EFI_DEVICE_PATH *devPath)
+{
+    BOOLEAN match = FALSE;
+    UINTN length = DevicePathNodeLength(imgPath);
+
+    if (imgPath->Type == devPath->Type) {
+        if (imgPath->SubType == devPath->SubType) {
+            if (length == DevicePathNodeLength(devPath)) {
+                if (Memcmp(imgPath, devPath, length) == 0) {
+                    match = TRUE;
+                }
+            }
+        }
+    }
+
+    return match;
+}
+
+
 EFI_STATUS
 efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *table)
 {
@@ -31,7 +69,7 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *table)
     EFI_STATUS status;
     EFI_HANDLE *handles;
     EFI_LOADED_IMAGE *img;
-    EFI_DEVICE_PATH *imgPath;
+    EFI_DEVICE_PATH *keepImgPath, *imgPath, *devPath;
     EFI_BLOCK_IO *blkio;
     EFI_DEVICE_PATH_TO_TEXT_PROTOCOL *text;
     CHAR16 *disp;
@@ -62,6 +100,7 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *table)
         Print(L"Loader path:%s\n", disp);
         status = uefi_call_wrapper(BS->HandleProtocol, 3, img->DeviceHandle, &DevicePathGUID, (VOID **)&imgPath);
         if (status != EFI_SUCCESS) {
+            keepImgPath = imgPath;
             goNext = FALSE;
         }
     }
@@ -79,7 +118,6 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *table)
 
     if (goNext == TRUE) {
         status = uefi_call_wrapper(BS->LocateHandle, 5, ByProtocol, &BlockIoProtocolGUID, NULL, &size, handles);
-        Print(L"+++ %d +++\n", status);
         switch (status) {
             case EFI_SUCCESS:
                 break;
@@ -104,6 +142,31 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *table)
     if (goNext == TRUE) {
         nHanles = size / sizeof(EFI_HANDLE);
         Print(L"Probing %u block devices...", nHanles);
+        for (UINTN i = 0; i < nHanles; ++i) {
+            status = uefi_call_wrapper(BS->HandleProtocol, 3, handles[i], &DevicePathGUID, (VOID **)&devPath);
+            if (status == EFI_SUCCESS) {
+                status = uefi_call_wrapper(BS->HandleProtocol, 3, handles[i], &BlockIoProtocolGUID, (VOID **)&blkio);
+                if (blkio->Media->LogicalPartition == TRUE) {
+                    BOOLEAN match = FALSE;
+                    imgPath = keepImgPath;
+                    while ((IsDevicePathEnd(imgPath) == FALSE) && (IsDevicePathEnd(devPath) == FALSE)) {
+                        UINTN length;
+                        if (IsDevicePathType(imgPath, MEDIA_DEVICE_PATH)) {
+                            if (IsDevicePathType(devPath, MEDIA_DEVICE_PATH)) {
+                                match = TRUE;
+                                break;
+                            }
+                        }
+                        if (IsNodeMatch(imgPath, devPath) == FALSE) {
+                            break;
+                        }
+
+                        imgPath = NextDevicePathNode(imgPath);
+                        devPath = NextDevicePathNode(devPath);
+                    }
+                }
+            }
+        }
     }
 
     for (;;) ;
