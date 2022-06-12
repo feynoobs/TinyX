430e0b3dd0bf786338f5996a5012435aa3ea0822
feynoobs <p.w.feeniej@gmail.com>
2022-06-12 16:03:13 +0900

    aaa

--- boot/boot.c
+++ boot/boot.c
@@ -8,6 +8,17 @@ Malloc(EFI_BOOT_SERVICES *BS, UINT64 length)
     EFI_STATUS status;
 
     status = uefi_call_wrapper(BS->AllocatePool, 2, length, (VOID **)&memory);
+    if (status != EFI_SUCCESS) {
+        memory = NULL;
+    }
+
+    return memory;
+}
+
+VOID
+Free(EFI_BOOT_SERVICES *BS, VOID *memory)
+{
+    uefi_call_wrapper(BS->FreePool, 1, memory);
 }
 
 EFI_STATUS
@@ -18,12 +29,14 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *table)
     EFI_BOOT_SERVICES *BS = table->BootServices;
     EFI_RUNTIME_SERVICES *RS = table->RuntimeServices;
     EFI_STATUS status;
-    EFI_HANDLE handle;
+    EFI_HANDLE *handles;
     EFI_LOADED_IMAGE *img;
     EFI_DEVICE_PATH *imgPath;
     EFI_BLOCK_IO *blkio;
     EFI_DEVICE_PATH_TO_TEXT_PROTOCOL *text;
     CHAR16 *disp;
+    UINTN size;
+    BOOLEAN goNext;
 
     EFI_GUID LoadedImageGUID = LOADED_IMAGE_PROTOCOL;
     EFI_GUID DevicePathGUID = DEVICE_PATH_PROTOCOL;
@@ -31,17 +44,58 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *table)
     EFI_GUID DevicePathToTextGUID = EFI_DEVICE_PATH_TO_TEXT_PROTOCOL_GUID;
 
     Print(L"TinyX Boot Loader\n");
+    goNext = TRUE;
     status = uefi_call_wrapper(BS->HandleProtocol, 3, image, &LoadedImageGUID, (VOID **)&img);
-    if (status == EFI_SUCCESS) {
+    if (status != EFI_SUCCESS) {
+        goNext = FALSE;
+    }
+
+    if (goNext == TRUE) {
         status = uefi_call_wrapper(BS->LocateProtocol, 3, &DevicePathToTextGUID, NULL, (VOID **)&text);
-        if (status == EFI_SUCCESS) {
-            disp = (CHAR16 *)uefi_call_wrapper(text->ConvertDevicePathToText, 3, img->FilePath, TRUE, TRUE);
-            Print(L"Loader path:%s\n", disp);
-            status = uefi_call_wrapper(BS->HandleProtocol, 3, img->DeviceHandle, &DevicePathGUID, (VOID **)&imgPath);
-            if (status == EFI_SUCCESS) {
-                disp = (CHAR16 *)uefi_call_wrapper(text->ConvertDevicePathToText, 3, imgPath, TRUE, TRUE);
-                Print(L"Load Device:%s\n", disp);
-            }
+        if (status != EFI_SUCCESS) {
+            goNext = FALSE;
+        }
+    }
+
+    if (goNext == TRUE) {
+        disp = (CHAR16 *)uefi_call_wrapper(text->ConvertDevicePathToText, 3, img->FilePath, TRUE, TRUE);
+        Print(L"Loader path:%s\n", disp);
+        status = uefi_call_wrapper(BS->HandleProtocol, 3, img->DeviceHandle, &DevicePathGUID, (VOID **)&imgPath);
+        if (status != EFI_SUCCESS) {
+            goNext = FALSE;
+        }
+    }
+
+    if (goNext == TRUE) {
+        disp = (CHAR16 *)uefi_call_wrapper(text->ConvertDevicePathToText, 3, imgPath, TRUE, TRUE);
+        Print(L"Load Device:%s\n", disp);
+        size = sizeof(EFI_HANDLE);
+        handles = (EFI_HANDLE *)Malloc(BS, size);
+        if (handles == NULL) {
+            goNext = FALSE;
+        }
+    }
+
+    if (goNext == TRUE) {
+        status = uefi_call_wrapper(BS->LocateHandle, 5, ByProtocol, &BlockIoProtocolGUID, NULL, &size, handles);
+        switch (status) {
+            case EFI_SUCCESS:
+                break;
+            case EFI_BUFFER_TOO_SMALL:
+                Free(BS, (VOID *)handles);
+                handles = Malloc(BS, size);
+                if (handles == NULL) {
+                    goNext = FALSE;
+                }
+                else {
+                    status = uefi_call_wrapper(BS->LocateHandle, 5, ByProtocol, &BlockIoProtocolGUID, NULL, &size, handles);
+                    if (status != EFI_SUCCESS) {
+                        goNext = FALSE;
+                    }
+                }
+                break;
+            default:
+                goNext = FALSE;
         }
     }
 
